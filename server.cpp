428b36cc2d921662ca70ab4a278b89d21946e4d0
Aaron B <aranbateni@gmail.com>
2021-07-30 04:25:19 +0430

    added the example server

---
+++ server.cpp
@@ -0,0 +1,16 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
+using namespace httplib;
+using namespace std;
+
+int main() {
+    httplib::Server svr;
+    std::cerr << "TEST1" << std::endl;
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+    res.set_content("Hello World!", "text/plain");
+    });
+    std::cerr << "TEST2" << std::endl;
+    svr.listen("0.0.0.0", 8080);
+    return 1;
+}
