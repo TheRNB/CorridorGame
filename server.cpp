9dabd2bc6ec34f083d0e17fa58fecfa2d85ae8e6
Aaron B <aranbateni@gmail.com>
2021-07-30 23:09:11 +0430

    added "/makeMove"

--- server.cpp
+++ server.cpp
@@ -24,6 +24,32 @@ int main() {
         res.set_content(response, "success");
     });
 
+    svr.Post("/makeMove", [&](const Request& req, Response& res) {
+        bool isPossible;
+        if (req.has_file("walk")) {
+            const auto& file = req.get_file_value("walk");
+            Direction drr;
+            if (file.content[1] == 'u') drr = UP;
+            if (file.content[1] == 'd') drr = DOWN;
+            if (file.content[1] == 'l') drr = LEFT;
+            if (file.content[1] == 'r') drr = RIGHT;
+            
+            isPossible = game.setPlayer(game.getPlayer(file.content[0]), drr);
+        } else if (req.has_file("block")) {
+            const auto& file = req.get_file_value("block");
+            Direction drr;
+            if (file.content[1] == 'v') drr = UP;
+            if (file.content[1] == 'h') drr = LEFT;
+            
+            int X = file.content[2];
+            int Y = file.content[3];
+
+            isPossible = game.setBlock(X, Y, drr);
+        }
+        string response = (((isPossible == true)?"1":"0"));
+        res.set_content(response, "success");
+    });
+
     svr.listen("localhost", 8080);
     return 0;
 }
